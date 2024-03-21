2e43ad4aaddedfd67495f184361b229dcd5f9f3b
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2024-03-21 11:45:30 -0400

    it2

---
+++ 3.2.3/intro.py
@@ -0,0 +1,24 @@
+# Basic FOR loop
+
+#$Looping through a specific list
+num_list = [42, 6, 11, 14, 7, 89, 17]
+'''''''
+for numbers in num_list:
+    print(numbers - 12)
+
+#looping a specified number of times
+for battered in range(1,20):
+    print(battered)
+
+for battered in range(20,0,-1):
+    print(battered)
+
+for battered in range(2, 11, 2):
+    print(battered)
+
+for battered in range(1,10,2):
+    print(battered)
+'''''''
+while shnuts < 11:
+    print(shnuts)
+    shnuts += 1
